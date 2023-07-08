78b723896ec8cded61883ce39af6f655f05a50f3
SHP-log <mp.byshp@gmail.com>
2023-07-08 18:25:12 +0900

    componentsフォルダ作成, not-foundファイル作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/page";
+
+const not_found = () => {
+  return (
+    <div>
+      <p>not_found</p>
+    </div>
+  );
+};
+
+export default not_found;
